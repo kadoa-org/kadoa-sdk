9f50beba898fa950bf9e26f63ded9a923e6bba89
David Barrat <david@barrat.io>
2025-01-03 12:28:00 +0100

    chore: remove duplicated types

--- node/src/index.ts
+++ node/src/index.ts
@@ -1,8 +1,3 @@
-export interface IKadoaProps {
-  apiKey?: string;
-  teamApiKey?: string;
-}
-
 const WSS_API_URI = process.env.WSS_KADOA_API_URI ?? "wss://realtime.kadoa.com";
 const PUBLIC_API_URI =
   process.env.PUBLIC_KADOA_API_URI ?? "https://api.kadoa.com";
